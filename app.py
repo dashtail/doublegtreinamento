61e9caca418795751a9c0778511bbb4ceba3d009
pseudonerds <ruanluiz.contato@gmail.con>
2018-12-19 22:55:36 -0200

    criando json do objeto

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json 
 
 class Evento():
     """Isso é a classe do Evento"""
@@ -8,9 +9,15 @@ class Evento():
         self.data = data
         self.organizador = ""
 
+
 festa_no_ap = Evento("festa no ap", "01/01/2019")
 festa_no_ap.organizador = "pseudonerds"
 
+#__dict__ pq sim
+json_festa = json.dumps(festa_no_ap.__dict__)
+
+print(json_festa)
+
 #exibo a classe
-print(festa_no_ap)
+#print(festa_no_ap)
 
